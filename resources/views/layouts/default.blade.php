0e6b11735b7d886a48765af06634e4d9fbfa4deb
songanqi@baojia.com <songanqi@baojia.com>
2019-01-23 16:54:58 +0800

    用户注册

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,11 +2,11 @@
 <html>
 <head>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>@yield('title', 'wuniantang app') - 宋安琪</title>
+    <title>@yield('title', 'wuniantang app') - 无念堂</title>
 </head>
 <body>
     @include('layouts._header')
-
+    @include('shared._messages')
     <div class="container">
         @yield('content')
     </div>
